4625aca8da6c5557d0f30e27162dc56c38c4fb58
ryoryou <atsut001@cougars.csusm.edu>
2016-04-21 10:43:49 -0700

    huhaha

--- test.cpp
+++ test.cpp
@@ -1,7 +1,11 @@
 #include<iostream>
 
+int sum(int num1, int num2){
+  return num1 + num2;
+}
 
 int main(){
-  std::cout << mult(2,3) << std::endl;
+  std::cout << "Mult: " << mult(2,3) << std::endl;
+  std::cout << "Sum: " << sum(3,4) << std::endl;
   return 0;
 }
