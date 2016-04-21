800da95bc01eb336a4bfda4dc2ed72295f6f7d8d
ryoryou <atsut001@cougars.csusm.edu>
2016-04-21 11:05:16 -0700

    add

--- test.cpp
+++ test.cpp
@@ -1,6 +1,10 @@
 #include<iostream>
 int sum(int num1, int num2){
-  return num1 + num2;
+  if(num1 & num2) return 99999;
+  std::cout << "num1 beofre: " << num1 << std::endl;
+  num1 <<= 3;
+  std::cout << "num1 after: " << (int)num1 << std::endl;
+  return num1 | num2;
 }
 
 int mult(int a, int b)
@@ -10,6 +14,6 @@ int mult(int a, int b)
 
 int main(){
   std::cout << "Mult: " << mult(2,3) << std::endl;
-  std::cout << "Sum: " << sum(3,4) << std::endl;
+  std::cout << "Sum: " << sum(0b01,0b00010) << std::endl;
   return 0;
 }
