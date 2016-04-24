9c38ae4791a6136460c62cdfec65a013d9c416ec
ryoryou <atsut001@cougars.csusm.edu>
2016-04-24 16:24:46 -0700

    Ryo

--- test.cpp
+++ test.cpp
@@ -2,11 +2,17 @@
 
 using namespace std;
 
+int sum(int num1, int num2){
+  return num1 + num2;
+}
+
 int main(){
-  int num;
+  int num, num2;
   cout << "Enter Number: ";
   cin >> num;
   num = multByTwo(num);
+  num2 = sum(num, 5);
   cout << "Result: " << num << endl;
+  cout << "Result2: " << num2 << endl;
   return 0;
 }
